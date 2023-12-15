bde6d1d0e515509ae4c588ce17f8401e91fd21c9
zaitkulov <zaitkulov@gmail.com>
2023-12-15 15:52:02 +0500

    HomeWork-6-2.Перечисления действий с автомобилем

--- HomeWork-6/main.swift
+++ HomeWork-6/main.swift
@@ -31,3 +31,41 @@ var mySportCar = SportCar(brand: "Lamborgini", year: 2023, trunkVolume: 0.5, eng
 
 var myTrunkCar = TrunkCar(brand: "Mercedes", year: 2020, trunkVolume: 10, engineRunning: true, windowsOpen: true, currentTrunkVolume: 5.0)
 
+//2.Задание: Создайте перечисления действий с автомобилем
+
+enum CarAction {
+    case startEngine
+    case stopEngine
+    case openWindows
+    case closeWindows
+    case loadCargo(volume: Int)
+    case unloadCargo(volume: Int)
+    
+    var availableCargoVolume: Int {
+        return 20
+    }
+}
+
+var myCar = CarAction.startEngine
+print (myCar)
+
+myCar = CarAction.loadCargo(volume: 21)
+print(myCar)
+
+switch myCar {
+case.startEngine:
+    print("Двигатель запущен")
+case.stopEngine:
+    print("Двигатель заглушен")
+case.openWindows:
+    print("Окна открыты")
+case.closeWindows:
+    print("Окна закрыты")
+case.loadCargo(let volume) where volume <= myCar.availableCargoVolume:
+    print ("Груз массой \(volume) тонн - загружен")
+case.loadCargo(let volume):
+    print ("Перегруз! Невозможно позгрузить \(volume) тонн")
+case.unloadCargo(let volume):
+    print ("Груз массой \(volume) успешно загружен")
+}
+
