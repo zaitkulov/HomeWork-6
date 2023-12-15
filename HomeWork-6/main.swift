2c68501c46382ba64f2cc20fd32955342a431118
zaitkulov <zaitkulov@gmail.com>
2023-12-15 16:14:06 +0500

    HomeWork-6-3.Добавление метода для изменения свойств авто

--- HomeWork-6/main.swift
+++ HomeWork-6/main.swift
@@ -69,3 +69,58 @@ case.unloadCargo(let volume):
     print ("Груз массой \(volume) успешно загружен")
 }
 
+//3.Задание: Добавление метода для изменения свойств автомобиля
+
+enum CarAction {
+    case startEngine
+    case stopEngine
+    case loadCargo(volume: Int)
+    case unloadCargo(volume: Int)
+}
+struct Car {
+    var isEngineRunning = false
+    var trunkCapacity = 40
+    mutating func performAction(_ action: CarAction) {
+        switch action {
+        case.startEngine:
+            if !isEngineRunning {
+                isEngineRunning = true
+                print ("Двигатель запущен")
+            } else {
+                print ("Двигатель уже запущен")
+            }
+        case.stopEngine:
+            if isEngineRunning {
+                isEngineRunning = false
+                print("Двигатель заглушен")
+            } else {
+                print ("Двигатель уже заглушен")
+            }
+        case.loadCargo(let volume):
+            if isEngineRunning {
+                if trunkCapacity >= volume {
+                    trunkCapacity -= volume
+                    print ("Погрузка завершена! Остаточное место в багажнике - \(trunkCapacity) тонн")
+                } else {
+                    print ("Перегруз! Недостаточно места в грузовом отсеке")
+                }
+            } else {
+                print ("Запустите двигатель, чтобы загрузить груз")
+            }
+        case.unloadCargo(let volume):
+            if isEngineRunning {
+                trunkCapacity += volume
+                print ("Выгрузка завершена! Остаточное место в грузовом отсеке - \(trunkCapacity) тонн")
+            } else {
+                print ("Запустите двигатель, для начала погрузки")
+            }
+        }
+    }
+}
+var myCar = Car()
+myCar.performAction(.startEngine)
+myCar.performAction(.loadCargo(volume: 40))
+myCar.performAction(.unloadCargo(volume: 35))
+myCar.performAction(.stopEngine)
+
+
